0caa2a42f4692dc8b5abeda406b2b187883b50a6
YouKnow <hsn123.h7@gmail.com>
2024-01-18 15:57:37 +0300

    minor changes

--- membership_portal/be_api_members/admin.py
+++ membership_portal/be_api_members/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Country, Organization
+from .models import Country, Organization,Profile
 
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(Organization)
+admin.site.register(Profile)
