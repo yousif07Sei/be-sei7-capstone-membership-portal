0fddd931d1b0164195137b1e3e8f88382458dbab
yousif <yousif.dhneem@gmail.com>
2024-01-18 18:22:07 +0300

    modified plan

--- membership_portal/be_api_members/models.py
+++ membership_portal/be_api_members/models.py
@@ -70,6 +70,10 @@ class Organization(models.Model):
     def __str__(self):
         print(self.name)
         return self.name
+    
+# class PlanFeature(models.Model):
+#     name = models.CharField(max_length=20)
+    
 class Plan(models.Model):
     name =models.CharField(max_length=100)
     price = models.DecimalField()
