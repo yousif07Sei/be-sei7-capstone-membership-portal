58ccabd02bbcdd18a1ac6a36734e69e044e8ff5d
sara-husain <sarahusain2428@gmail.com>
2024-01-18 09:14:10 +0300

    OrgModel and installed REST

--- membership_portal/be_api_members/models.py
+++ membership_portal/be_api_members/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    phoneNumber = models.CharField(max_length=100)
+    emailAddress = models.EmailField(max_length=100)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
