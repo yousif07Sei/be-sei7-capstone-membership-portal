828328633b3b6ea362d08a0dc5d303841d121563
sara-husain <sarahusain2428@gmail.com>
2024-01-18 10:49:11 +0300

    removed and added org type

--- membership_portal/be_api_members/models.py
+++ membership_portal/be_api_members/models.py
@@ -5,6 +5,7 @@ class Organization(models.Model):
     name = models.CharField(max_length=100)
     phoneNumber = models.CharField(max_length=100)
     emailAddress = models.EmailField(max_length=100)
+    type = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
