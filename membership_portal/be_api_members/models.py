a53f5812488a3fb1b737fe4d55f8c3abe30324ee
alij <alimjahrami@gmail.com>
2024-01-18 10:16:29 +0300

    add country model

--- membership_portal/be_api_members/models.py
+++ membership_portal/be_api_members/models.py
@@ -9,3 +9,20 @@ class Organization(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+    short_name = models.CharField(max_length=20)
+    nationality_name = models.CharField(max_length=40)
+    flag = models.CharField(max_length=200)
+    capital = models.CharField(max_length=200)
+    currency = models.CharField(max_length=100)
+    currency_sign = models.CharField(max_length=20)
+    country_code = models.CharField(max_length=4)
+    listed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Countries'
