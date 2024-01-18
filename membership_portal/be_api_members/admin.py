79141037c06fa63dd6845efcbcffed0c9680b9e6
alij <alimjahrami@gmail.com>
2024-01-18 13:45:13 +0300

    modify admin fields and add search

--- membership_portal/be_api_members/admin.py
+++ membership_portal/be_api_members/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Country, Organization
 
+# Modify fields in admin panel
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'short_name', 'listed']
+    search_fields = ["name"]
+    search_help_text = 'Search by Country Name'
+
 # Register your models here.
-admin.site.register(Country)
+admin.site.register(Country, CountryAdmin)
 admin.site.register(Organization)
