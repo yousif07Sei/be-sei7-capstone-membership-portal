7c244405ce8fe745b51c378d30e15daa250c1916
Mazen Al-Maskati <m@mmt.me>
2024-01-21 18:28:46 +0300

    Profile Organization

--- membership_portal/fs_business_mvt/urls.py
+++ membership_portal/fs_business_mvt/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name="bussines_portal_app"
 urlpatterns = [
-    path('home/',views.home,name='home'),
+    path('',views.home, name='home'),
     path('organization/',views.OrganizationList.as_view(),name='organization_index'),
     path('organization/<int:pk>/update/',views.OrganizationUpdate.as_view(),name='organization_update'),
     path('organization/<int:pk>/delete/',views.OrganizationDelete.as_view(),name='organization_delete'),
