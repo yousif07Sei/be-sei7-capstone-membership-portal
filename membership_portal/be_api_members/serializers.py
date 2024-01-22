ed469309e7d23fd59b03aece7357e886f0535fac
alij <alimjahrami@gmail.com>
2024-01-22 16:49:52 +0300

    add enpoints to benefit API

--- membership_portal/be_api_members/serializers.py
+++ membership_portal/be_api_members/serializers.py
@@ -15,8 +15,17 @@ class BenefitRESTSerializers(serializers.Serializer):
     title = serializers.CharField(required = True)
     expiry_date = serializers.DateField()
     status = serializers.CharField()
+
     def create(self, validated_data):
         return Benefit.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.description = validated_data.get('description', instance.description)
+        instance.title = validated_data.get('title', instance.title)
+        instance.expiry_date = validated_data.get('expiry_date', instance.expiry_date)
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
+        return instance
 
 # class TestModelSerializer(serializers.ModelSerializer):
 #     # name = serializers.CharField(required = True)
